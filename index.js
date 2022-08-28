a0e1de7b16f706788e87830acaef257fcfe40f56
Hein Thant <heinthantx@gmail.com>
2022-08-28 10:01:55 +0630

    .

--- index.js
+++ index.js
@@ -41,11 +41,6 @@ const coinDropAudio = new Audio('./assets/audio/CoinGold AMA01_88_1.wav');
 const lossAudio = new Audio('./assets/audio/Stage Failed.wav');
 const winAudio = new Audio('./assets/audio/Happy Win Game.wav');
 
-let current = 0;
-const root = document.documentElement;
-const loadingBar = document.querySelector('.loading');
-const startLoading = document.querySelector('.start-loading');
-let id;
 function getImage(url) {
   return new Promise(function (resolve, reject) {
     var img = new Image();
@@ -56,37 +51,10 @@ function getImage(url) {
       reject(url);
     };
     img.src = url;
-  }).then(() => {
-    current += 3.8463;
-    clearInterval(id);
-    let getPropertyValue = parseFloat(root.style.getPropertyValue('--hell'));
-    id = setInterval(() => {
-      getPropertyValue += 0.1;
-      root.style.setProperty('--hell', getPropertyValue + '%');
-      console.log(getPropertyValue);
-      if (current < getPropertyValue) {
-        clearInterval(id);
-      }
-      if (Math.floor(getPropertyValue) == 100) {
-        clearInterval(id);
-        current = 0;
-        menuBoardContainer.style.display = 'none';
-        startLoading.style.visibility = 'hidden';
-        achievementContainer.style.display = 'none';
-        settingContainer.style.display = 'none';
-        profileContainer.style.display = 'none';
-        loadingBar.classList.remove('loadingAnimation');
-        root.style.setProperty('--hell', current + '%');
-        gameContainer.style.display = 'flex';
-        isPlay = false;
-      }
-    }, 1);
   });
 }
 
 function gameContainerPreloader() {
-  root.style.setProperty('--hell', 0 + '%');
-
   return Promise.all([
     getImage('./assets/images/square-buttons/sea-bg.png'),
     getImage('./assets/images/btn/play.png'),
@@ -489,7 +457,8 @@ function clearALlValue() {
 //   clearALlValue();
 // });
 
-let isPlay = false;
+let firstStart = true;
+let playBtnClicked = false;
 menuPlayBtn.addEventListener('click', () => {
   clickAudio.play();
   startAudio.play();
@@ -497,11 +466,48 @@ menuPlayBtn.addEventListener('click', () => {
   setTimeout(() => {
     menuPlayBtn.classList.remove('buttonAnimate');
   }, 310);
-  if (isPlay) return;
-  isPlay = true;
-  startLoading.style.visibility = 'visible';
-  loadingBar.classList.add('loadingAnimation');
-  gameContainerPreloader();
+  if (firstStart) {
+    if (playBtnClicked) return;
+    playBtnClicked = true;
+    const loadingBar = document.querySelector('.loading');
+    const startLoading = document.querySelector('.start-loading');
+    startLoading.style.visibility = 'visible';
+    loadingBar.classList.add('loadingAnimation');
+    const root = document.documentElement;
+    setTimeout(() => {
+      gameContainerPreloader()
+        .then(() => {
+          let percentage = 70;
+          new Promise((res, rej) => {
+            let progressStatusId = setInterval(() => {
+              percentage += 0.1;
+              root.style.setProperty('--hell', percentage + '%');
+              if (Math.floor(percentage) == 100) {
+                res();
+                clearInterval(progressStatusId);
+              }
+            }, 1);
+          }).then(() => {
+            menuBoardContainer.style.display = 'none';
+            startLoading.style.visibility = 'hidden';
+            achievementContainer.style.display = 'none';
+            settingContainer.style.display = 'none';
+            profileContainer.style.display = 'none';
+            loadingBar.classList.remove('loadingAnimation');
+            root.style.setProperty('--hell', 70 + '%');
+            gameContainer.style.display = 'flex';
+            firstStart = false;
+          });
+        })
+        .catch((e) => console.log(e));
+    }, 3100);
+  } else {
+    menuBoardContainer.style.display = 'none';
+    achievementContainer.style.display = 'none';
+    settingContainer.style.display = 'none';
+    profileContainer.style.display = 'none';
+    gameContainer.style.display = 'flex';
+  }
 });
 
 const volumeBtn = document.getElementById('volumeBtn');
